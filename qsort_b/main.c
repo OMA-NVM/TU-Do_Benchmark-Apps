009300cda8f1b374f77273d0a513b915ba8fa8bd
Minh Duy <minhduy.truong@tu-dortmund.de>
2022-08-29 14:05:52 +0000

    added missing memory region pointers to main file

--- qsort_b/main.c
+++ qsort_b/main.c
@@ -6,11 +6,36 @@
 #endif /* __Unikraft__ */
 #include "data.h"
 #include "qsort_b.h"
+#include "../regions_header/mem_regions.h"
+
+unsigned long __apptext_start;
+unsigned long __apptext_end;
+unsigned long __approdata_start;
+unsigned long __approdata_end;
+unsigned long __appdata_start;
+unsigned long __appdata_end;
+unsigned long __appbss_start;
+unsigned long __appbss_end;
+unsigned long __appstack_start;
+unsigned long __appstack_end;
+unsigned long __appheap_start;
+unsigned long __appheap_end;
 
 int main(int argc, char *argv[])
 {
-
-
+    printf("__apptext_start: 0x%lx\n", &__apptext_start);
+	printf("__apptext_end: 0x%lx\n", &__apptext_end);
+	printf("__approdata_start: 0x%lx\n", &__approdata_start);
+	printf("__approdata_end: 0x%lx\n", &__approdata_end);
+	printf("__appdata_start: 0x%lx\n", &__appdata_start);
+	printf("__appdata_end: 0x%lx\n", &__appdata_end);
+	printf("__appbss_start: 0x%lx\n", &__appbss_start);
+	printf("__appbss_end: 0x%lx\n", &__appbss_end);
+	printf("__appheap_start: 0x%lx\n", &__appheap_start);
+	printf("__appheap_end: 0x%lx\n", &__appheap_end);
+	printf("__appstack_start: 0x%lx\n", &__appstack_start);
+	printf("__appstack_end: 0x%lx\n", &__appstack_end);
+    
 	printf("Hello qsort_b!\n");
 	app_init();
 	printf("Goodbye qsort_b!\n");
--- stringsearch/main.c
+++ stringsearch/main.c
@@ -30,6 +30,20 @@ e.g bmhi_init" and  "bmhi_search" instead*/
 #include "bmhsrch.h"
 //#include "input_small.h"
 #include "input_large.h"
+#include "../regions_header/mem_regions.h"
+
+unsigned long __apptext_start;
+unsigned long __apptext_end;
+unsigned long __approdata_start;
+unsigned long __approdata_end;
+unsigned long __appdata_start;
+unsigned long __appdata_end;
+unsigned long __appbss_start;
+unsigned long __appbss_end;
+unsigned long __appstack_start;
+unsigned long __appstack_end;
+unsigned long __appheap_start;
+unsigned long __appheap_end;
 
 static size_t table[UCHAR_MAX + 1];
 static size_t len;
@@ -85,7 +99,18 @@ char *strsearch(const char *string)
 
 int main(int argc, char *argv[])
 {
-
+      printf("__apptext_start: 0x%lx\n", &__apptext_start);
+	printf("__apptext_end: 0x%lx\n", &__apptext_end);
+	printf("__approdata_start: 0x%lx\n", &__approdata_start);
+	printf("__approdata_end: 0x%lx\n", &__approdata_end);
+	printf("__appdata_start: 0x%lx\n", &__appdata_start);
+	printf("__appdata_end: 0x%lx\n", &__appdata_end);
+	printf("__appbss_start: 0x%lx\n", &__appbss_start);
+	printf("__appbss_end: 0x%lx\n", &__appbss_end);
+	printf("__appheap_start: 0x%lx\n", &__appheap_start);
+	printf("__appheap_end: 0x%lx\n", &__appheap_end);
+	printf("__appstack_start: 0x%lx\n", &__appstack_start);
+	printf("__appstack_end: 0x%lx\n", &__appstack_end);
 
 	printf("Hello stringsearch!\n");
 
