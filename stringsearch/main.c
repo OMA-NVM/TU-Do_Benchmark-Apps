6f500c604aa78a44370544130d164653a08836f6
Tristan Seidl <tristan.seidl@tu-dortmund.de>
2023-07-11 15:14:30 +0200

    Fixed asm stack pointer change.

--- stringsearch/main.c
+++ stringsearch/main.c
@@ -126,7 +126,7 @@ int main(int argc, char *argv[]) {
         "ldp x6,x7,[sp],#16;"
         "ldp x4,x5,[sp],#16;"
         "ldp x2,x3,[sp],#16;"
-        "ldp x0,x1,[sp],#16;" :: "r"(correctedAppStackEnd), "r"(mainApp), "r"(&osSP) : "x0"
+        "ldp x0,x1,[sp],#16;" :: "r"(correctedAppStackEnd), "r"(mainApp), "r"(&osSP) : "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x30"
 	);
 }
 
